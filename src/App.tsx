a5e37b459b3834609d44300f4cdb4eeb96b6149c
smallshaq18 <abdulsamad.m1802472@gmail.com>
2026-03-07 18:03:15 +0100

    removed other useless files

--- src/App.tsx
+++ src/App.tsx
@@ -35,32 +35,3 @@ function App() {
 }
 
 export default App;
-
-
-/*import { Routes, Route } from "react-router-dom";
-import "./index.css";
-import Home from "./pages/Home";
-import Game from "./pages/Game";
-import IntroScreen from "./components/IntroScreen";
-import { useState } from "react";
-
-function App() {
-
-  const [started, setStarted] = useState(false);
-
-  if (!started) {
-    return <IntroScreen onStart={() => setStarted(true)} />;
-  }
-  
-  return (
-    <div className="flex items-center justify-center bg-gradient-to-br from-red-500 to-purple-600">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/game" element={<Game />} />
-      </Routes>
-    </div>
-  );
-}
-
-export default App;*/
-
